a1633f812ec32796a7b98530c0050ec38c0d6fe5
Barış Bükümcüler <barisbukumculer@gmail.com>
2024-05-13 03:16:04 +0300

    Admin e-mail sending operations and logout operations

---
+++ SignalRWebUI/Controllers/MailController.cs
@@ -0,0 +1,43 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using SignalRWebUI.Dtos.MailDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CreateMailDto createMailDto)
+        {
+            MimeMessage mimeMessage=new MimeMessage();
+
+            MailboxAddress mailboxAddress = new MailboxAddress("SignalR Rezervasyon","SENDERMAIL");
+            mimeMessage.From.Add(mailboxAddress);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", createMailDto.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody= createMailDto.Body;
+            mimeMessage.Body=bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject=createMailDto.Subject;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("SENDERMAIL","KEY");
+
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+
+            return RedirectToAction("Index", "Category");
+        }
+    }
+}
